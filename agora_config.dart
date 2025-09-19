92524492a820d57de8086df0cb57f9cf84d4ea80
labibanis786 <147445808+labibanis786@users.noreply.github.com>
2025-09-19 18:30:20 +0530

    Add files via upload

---
+++ agora_config.dart
@@ -0,0 +1,3 @@
+class AgoraConfig {
+  static const String appId = "739a61014ae64d2eaf7dd9eb4872664e";
+}
---
+++ main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Big Fan Voice Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Big Fan Voice Chat"),
+      ),
+      body: const Center(
+        child: Text("Welcome to Big Fan Voice Chat!"),
+      ),
+    );
+  }
+}
