52739aace36dd8a296d485bbabfe9c31da0519a7
labibanis786 <147445808+labibanis786@users.noreply.github.com>
2025-09-19 19:46:06 +0530

    add agora config file

---
+++ lib/agora_config.dart
@@ -0,0 +1,8 @@
+// Your Agora App ID goes here
+const String agoraAppId = "739a61014ae64d2eaf7dd9eb4872664e";
+
+// Token (if you are using a temporary token, put it here)
+const String agoraToken = "";
+
+// Default channel name
+const String defaultChannel = "bigfun_voice_chat";
